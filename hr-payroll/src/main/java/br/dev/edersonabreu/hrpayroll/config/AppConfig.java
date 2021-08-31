10eaae5dde3f94b6ed2b6bda698c9a2250c0dc6c
Ederson Abreu <ederson.dimm@gmail.com>
2021-08-31 16:10:13 -0300

    Projects hr-worker and hr-payroll implemented (eurekaClient)

--- hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/config/AppConfig.java
+++
@@ -1,8 +0,0 @@
-package br.dev.edersonabreu.hrpayroll.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-	
-}
--- hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/feignClients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/feignClients/WorkerFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import br.dev.edersonabreu.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
 	@GetMapping
