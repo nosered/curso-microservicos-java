8cc643a74e8736ea14739b7b9d869aea7c2ff4f6
Ederson Abreu <ederson.dimm@gmail.com>
2021-08-31 13:07:35 -0300

    Project hr-payroll implemented (feignClient)

--- hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/config/AppConfig.java
@@ -1,14 +1,8 @@
 package br.dev.edersonabreu.hrpayroll.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+	
 }
--- hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/services/PaymentService.java
@@ -1,37 +1,20 @@
 package br.dev.edersonabreu.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.dev.edersonabreu.hrpayroll.entities.Payment;
 import br.dev.edersonabreu.hrpayroll.entities.Worker;
+import br.dev.edersonabreu.hrpayroll.feignClients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value(value = "${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		Map<String, String> uriVariables = new HashMap<String, String>();
-		uriVariables.put("id", workerId.toString());
-		
-		String uri = new StringBuilder()
-							.append(workerHost)
-							.append("/workers")
-							.append("/{id}")
-							.toString();
-		
-		Worker worker = restTemplate.getForObject(uri, Worker.class, uriVariables);
-		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
