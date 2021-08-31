f0cf22b1a792d953b6c6bbbc464ac73976ce93a8
Ederson Abreu <ederson.dimm@gmail.com>
2021-08-31 10:19:53 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/br/dev/edersonabreu/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.edersonabreu.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
