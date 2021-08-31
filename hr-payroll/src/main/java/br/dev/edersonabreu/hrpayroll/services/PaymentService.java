562e9944085da07436cbb8f19e7a2a2f21e0744e
Ederson Abreu <ederson.dimm@gmail.com>
2021-08-31 11:11:09 -0300

    Project hr-payroll implemented (restTemplate)

--- hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/dev/edersonabreu/hrpayroll/services/PaymentService.java
@@ -1,13 +1,37 @@
 package br.dev.edersonabreu.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.dev.edersonabreu.hrpayroll.entities.Payment;
+import br.dev.edersonabreu.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value(value = "${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("id", workerId.toString());
+		
+		String uri = new StringBuilder()
+							.append(workerHost)
+							.append("/workers")
+							.append("/{id}")
+							.toString();
+		
+		Worker worker = restTemplate.getForObject(uri, Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
