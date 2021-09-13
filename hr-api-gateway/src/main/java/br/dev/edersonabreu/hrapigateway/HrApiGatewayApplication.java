05a8947cbd3c982acc2fc62b8ab35295c5dbb4c3
Ederson Abreu <ederson.dimm@gmail.com>
2021-09-13 09:48:53 -0300

    Project hr-api-gateway implemented

--- hr-api-gateway/src/main/java/br/dev/edersonabreu/hrapigateway/HrApiGatewayApplication.java
+++ hr-api-gateway/src/main/java/br/dev/edersonabreu/hrapigateway/HrApiGatewayApplication.java
@@ -1,10 +1,35 @@
 package br.dev.edersonabreu.hrapigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class HrApiGatewayApplication {
+	
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+	    return builder.routes()
+	    			.route(p -> p
+	    				.path("/hr-worker/**")
+	    				.filters(f -> f.rewritePath("/hr-worker/(?<path>.*)", "/$\\{path}"))
+	    				.uri("lb://hr-worker")
+	    			)
+	    			.route(p -> p
+	    				.path("/hr-payroll/**")
+	    				.filters(f -> f.rewritePath("/hr-payroll/(?<path>.*)", "/$\\{path}"))
+	    				.uri("lb://hr-payroll")
+	    			)
+	    			.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrApiGatewayApplication.class, args);
